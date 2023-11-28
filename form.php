221b549be958758947ca030d47f7037db2d010de
Alek Miko <alekmikopro@gmail.com>
2023-11-28 12:39:18 +0100

    adding the file to display the data

--- form.php
+++ form.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form  action="form.php"  method="post">
+    <form  action="display-info.php"  method="post">
         <div>
             <label  for="nom">Nom :</label>
             <input  type="text"  id="nom"  name="user_lastname">
@@ -46,21 +46,3 @@
     </form>
 </body>
 </html>
-<?php
-    $firstname =  $_POST['user_firstname'];
-    $lastname =  $_POST['user_lastname'];
-    $email =  $_POST['user_email'];
-    $phone =  $_POST['user_phone_number'];
-    $subject =  $_POST['user_subject'];
-    $message =  $_POST['user_message'];
-
-    echo nl2br("\n");
-    echo nl2br("\n");
-    echo  "Merci " . $firstname . " " . $lastname . " de nous avoir contacté à propos de " . $subject . ".";
-    echo nl2br("\n");
-    echo nl2br("\n");
-    echo "Un de nos conseillers vous contactera soit à l’adresse " . $email . " ou par téléphone au ". $phone . " dans les plus brefs délais pour traiter votre demande : ";
-    echo nl2br("\n");
-    echo nl2br("\n");
-    echo $message;
-?>
